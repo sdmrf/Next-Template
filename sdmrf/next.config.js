ed6fcd91d947f68d78c68610771c3889103b5e17
Syed Maruf Ali <syedmarufali23@gmail.com>
2023-10-12 11:59:45 +0530

    Updated

--- sdmrf/next.config.js
+++ sdmrf/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
 
-module.exports = nextConfig
+  // Add basePath
+  basePath: "/github-pages",
+};
+
+module.exports = nextConfig;
